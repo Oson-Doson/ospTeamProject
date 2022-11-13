1e099bd5eba7b302113cc25f9959b3685217bf19
wowalswjd <alswjd5491@naver.com>
2022-11-13 23:58:35 +0900

    allergy x버튼 추가, app.py 코드 추가

--- ospTeamProject/맛집등록/app.py
+++ ospTeamProject/맛집등록/app.py
@@ -12,6 +12,16 @@ def reg_restaurant_submit_post():
     data=request.form
     return render_template("result.html",data=data)
 
+@app.route('/menu_upload')
+def menuUpload():
+    return render_template("menuUpload.html")
+
+@app.route("/submit_menu_post", methods = ['POST'])
+def reg_menu_submit_post():
+    image_file=request.files["file"]
+    image_file.save("static/image/{}".format(image_file.filename))
+    data=request.form
+    return render_template("", data=data)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
