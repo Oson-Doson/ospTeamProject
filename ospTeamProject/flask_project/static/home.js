637e308428ef5e6d0ae0def47c394e3e8d215ac0
crHwang0822 <chaa00@naver.com>
2022-12-02 04:51:41 +0900

    홈화면 탭메뉴 화살표 누르면 탭 이동되도록 구현

--- ospTeamProject/flask_project/static/home.js
+++ ospTeamProject/flask_project/static/home.js
@@ -8,6 +8,105 @@ $(document).ready(function(){
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
-	})
+	})  
+
+    $('#next-slick-arrow').click(function(){
+        if($('.typeButton.current').attr('data-tab')=='tab-1'){
+            var $button=$('#typeKorean')
+            var tab_id='tab-2';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-2'){
+            var $button=$('#typeChinese')
+            var tab_id='tab-3';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-3'){
+            var $button=$('#typeJapanese')
+            var tab_id='tab-4';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-4'){
+            var $button=$('#typeWestern')
+            var tab_id='tab-5';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-5'){
+            var $button=$('#typeCafe')
+            var tab_id='tab-6';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+    })
+
+    $('#prev-slick-arrow').click(function(){
+    
+        if($('.typeButton.current').attr('data-tab')=='tab-2'){
+            var $button=$('#typeTotal')
+            var tab_id='tab-1';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-3'){
+            var $button=$('#typeKorean')
+            var tab_id='tab-2';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-4'){
+            var $button=$('#typeChinese')
+            var tab_id='tab-3';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-5'){
+            var $button=$('#typeJapanese')
+            var tab_id='tab-4';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+            $(this).style.display=none;
+        }
+        else if($('.typeButton.current').attr('data-tab')=='tab-6'){
+            var $button=$('#typeWestern')
+            var tab_id='tab-5';
+            $('.typeButton').removeClass('current');
+		    $('.tab-content').removeClass('current');
+
+		    $button.addClass('current');
+		    $("#"+tab_id).addClass('current');
+            $(this).style.display=none;
+        }
+    })
 
 })
