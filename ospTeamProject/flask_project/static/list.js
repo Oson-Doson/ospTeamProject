48e48ea2fc5164044d9a5384af7cbe1bf922311f
yeojinLee1020 <yeojinlee1020@gmail.com>
2022-12-06 01:37:08 +0900

    리스트화면 네비게이션 바 색깔 고정

--- ospTeamProject/flask_project/static/list.js
+++ ospTeamProject/flask_project/static/list.js
@@ -16,6 +16,22 @@ $(document).ready(function () {
         $('.typeButton').removeClass('current');
         $('#typeKorean').addClass('current');
     }
+    else if(type=='중식'){
+        $('.typeButton').removeClass('current');
+        $('#typeChinese').addClass('current');
+    }
+    else if(type=='일식'){
+        $('.typeButton').removeClass('current');
+        $('#typeJapanese').addClass('current');
+    }
+    else if(type=='양식'){
+        $('.typeButton').removeClass('current');
+        $('#typeWestern').addClass('current');
+    }
+    else if(type=='카페'){
+        $('.typeButton').removeClass('current');
+        $('#typeCafe').addClass('current');
+    }
 })
 
 //$('.page-nation a').attr('id', this.)
